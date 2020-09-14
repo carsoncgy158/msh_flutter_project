787d6877be391df446de06ea70e6fb1beb305414
carsoncgy158 <64817944+carsoncgy158@users.noreply.github.com>
2020-09-14 13:08:46 -0400

    Update server.dart

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -1,5 +1,5 @@
 const LEANCLOUD_SERVER_URL = 'https://api.munshare.com';
-const LEANCLOUD_APP_ID = 'thxE3ImtBCsojP7Hgh3C9T6a-gzGzoHsz';
-const LEANCLOUD_APP_KEY = 'KNSsuNPv7eB2f851IHdImdSW';
+const LEANCLOUD_APP_ID = '';
+const LEANCLOUD_APP_KEY = '';
 
 const SERVER_API_URL = 'https://api.munshare.com';
